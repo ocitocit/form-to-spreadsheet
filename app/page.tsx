3d96979a15a69053e410fe2c93fcd39f2da945f7
Abdul Rosyid <ocritocrit@gmail.com>
2023-08-08 08:57:22 +0700

    add some form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <main className="bg-gray-100 min-h-screen ">
+      <div className="max-w-5xl mx-auto py-16">
+        <form className="py-4 space-y-4">
+          <div className="flex item-center justify-center">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" type="name" />
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }

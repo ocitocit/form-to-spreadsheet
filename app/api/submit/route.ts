7e4271c727299dcdc63398ddecbcfb5c35142fb5
Abdul Rosyid <ocritocrit@gmail.com>
2023-08-13 05:17:45 +0700

    change dir

---
+++ app/api/submit/route.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { google } from "googleapis";
+
+type SheetForm = {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+};
+
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).send({ message: "only POST method are allowed" });
+  }
+
+  const body = req.body as SheetForm;
+
+  try {
+    //prepare auth
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/spreadsheets",
+      ],
+    });
+
+    const sheets = google.sheets({
+      auth,
+      version: "v4",
+    });
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "A1:D1",
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values: [[body.name, body.email, body.phone, body.message]],
+      },
+    });
+
+    // return res.status(200).json({ data: response.data });
+    return res.status(200).json({ data: response.data.updates?.updatedRange });
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).send({ message: "seems someting wont going well" });
+  }
+}
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
